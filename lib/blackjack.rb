31a65761505e43dec37a2d2144396a1a868b1801
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-09 22:11:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,9 +34,9 @@ end
 def hit?(card_total)
   prompt_user
   input = get_user_input
-  if input = h
+  if input == h
     card_total += deal_card
-  elsif input = s
+  elsif input == s
   else
     invalid_command
     prompt_user
