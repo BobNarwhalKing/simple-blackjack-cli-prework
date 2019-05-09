1ecaeb0b5b7e801ef0e8d9295d47f5d6c3136a7f
Kurtis Leach <danceflurry4366@gmail.com>
2019-05-09 22:23:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,5 +53,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  hand = initial_round
+  until hand > 21
+    hand = hit? (hand)
+    display_card_total(hand)
+  end
+end_game(hand)
 end
